d4f6822071a0d5bf91d8feb346b1f836983e6891
Breathing <1440422736@qq.com>
2019-09-17 23:52:53 +0800

    第一次写java代码

---
+++ hello world/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+	public static void main (String[] args){
+		System.out.println("HelloWorld");
+	}
+}public static void numberoverflow(){
+	int min = integer.MIN_VALUE
+	int max = integer.MAX_VALUE
+	System.out.println(min);
+	System.out.println(max);
+}
+
+
