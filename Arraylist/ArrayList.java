a30a997af61b68efc8bfc2b3e9307da4ee8feafd
Breathing <1440422736@qq.com>
2019-10-03 22:55:31 +0800

    ArrayList

---
+++ Arraylist/ArrayList.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Arraysays;
+public class ArrayList extends List {
+private  int[] arry =new int[10];
+
+    @Override
+    public void insert(int index, int element) {
+        for(int i =size ; i > index ; i--){
+        array [i] = array[i - 1];
+        }
+        array[index] = element;
+        size++;
+    }
+    int[] a=Arrays.cop
+}
+
+
+pravite static void swap (List<Card>deck ,int i int j){
+    Card t =deck.get(i);
+    deck.set()
+        }
+
+public static void main(String[]args){
+    List<Card> deck = buyDeck();
+    System.out.println(deck);
+    shuffle(deck);
+    List<List<Card>> hands = new ArrayList<>();
+    send(haands,deck,numperson:3,numperson:5);
+    System.out.println(hands.get(0));
+    System.out.println(hands.get(1));
+    System.out.println(hands.get(2));
+    System.out.println(deck);
+        }
+
+
+0
