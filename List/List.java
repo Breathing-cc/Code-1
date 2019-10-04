7dabe8e6ed4e9e324f0ff74b73617945834ab894
Breathing <1440422736@qq.com>
2019-10-04 23:20:20 +0800

    List

---
+++ List/List.java
@@ -0,0 +1,14 @@
+public class List{
+    protecteed int size = 0;
+    public void insert(int index,int element)
+    {
+        System.out.println("没实现" );
+    }
+    public void pushFront(int element){
+        insert(index:0,element);
+    }
+    public  void pushBack(int element){
+        insert(size , element);
+    }
+}
+
