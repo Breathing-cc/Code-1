93b538ba52c6eefce0cef13bcfb061787994f622
Breathing <1440422736@qq.com>
2019-10-05 22:54:01 +0800

    Static

---
+++ List/Static.java
@@ -0,0 +1,13 @@
+public class Static {
+    public interface IL inked{
+        void push(int val){
+            if(node = null){
+                return null;
+            }else{
+                Node =new node;
+                last = last.next;
+
+            }
+        }
+    }
+}
