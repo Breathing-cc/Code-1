bc37b5e8fad50b656fa60829d7dfb4cafbf68d70
Breathing <1440422736@qq.com>
2019-10-07 23:04:58 +0800

    equals

---
+++ Arraylist/equals.java
@@ -0,0 +1,12 @@
+class Atricle{
+       Strcing title;
+       String author;
+       String content;
+       String publishedAt;
+   }
+   public class equals {
+    if(obj = null){
+        return false;
+       }
+    if(!(obj instanceof Atricle))
+}
