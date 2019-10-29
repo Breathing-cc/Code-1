0f80c0039292956313cc367ab686d820e8db9b11
Breathing <1440422736@qq.com>
2019-10-29 23:42:46 +0800

    youmehe

---
+++ hello world/Youmehe.java
@@ -0,0 +1,10 @@
+public class Youmehe{
+	public static void main(String []args){
+		char a ='你';
+		char b ='我';
+		char c ='他';
+		System.out.println("'你'在Unicode表中的位置是:"+(int)a);
+		System.out.println("'我'在Unicode表中的位置是:"+(int)b);
+		System.out.println("'他'在Unicode表中的位置是:"+(int)c);
+	}
+}
