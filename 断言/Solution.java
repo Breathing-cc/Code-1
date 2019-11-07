ebe14227eada29558b6040cc245d656869cc260c
Breathing <1440422736@qq.com>
2019-11-07 23:56:29 +0800

    断言

---
+++ 断言/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        double sum = 0;
+        int m = 0;
+        while(reader.hasNextDouble()){
+            double x = reader.nextDouble();
+            assert m > 100 :"这是一个非法的成绩数据";
+            assert m < 0 :"这是一个非法的成绩数据";
+            m = m + 1;
+            sum = sum + x;
+        }
+        System.out.printf("%d个数的和为%f\n",m,sum);
+        System.out.printf("%d个数的平均值是%f\n",m,sum/m);
+    }
+}
